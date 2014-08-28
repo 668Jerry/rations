0415e78e897bb8cfb2368f9b0b5ada35822f1f99
AWS Server <668jerry@gmail.com>
2014-08-28 17:05:04 +0800

    init

---
+++ hk0weatherenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lololol/Documents/Python/g0v/hk0weather/hk0weatherenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
